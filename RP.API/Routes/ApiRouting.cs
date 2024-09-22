cc00e759098d6e26f9e6e13e564dcf6618d9c139
thang.le.van <thang.le.van@dmchosting.ca>
2024-09-22 21:20:26 +0700

    adđ

--- RP.API/Routes/ApiRouting.cs
+++ RP.API/Routes/ApiRouting.cs
@@ -27,6 +27,67 @@ namespace RP.API.Routes
                 await context.Response.WriteAsync($"Welcome Go Sell .NET! Version: {typeof(RP.API.Routes.ApiRouting).Assembly.GetName().Version}");
             });
         }
-      
+
+        public static RouteGroupBuilder MapAffiliateAuthencationApi(this RouteGroupBuilder app)
+        {
+            app.MapPost("/sendOTPByPhoneNumber", AffiliateAuthenticationApi.AffiliateSendSMSOTPAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/signin", AffiliateAuthenticationApi.AffiliateSigninAsync)
+               .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/signup", AffiliateAuthenticationApi.AffiliateSignupAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/verifyOtp", AffiliateAuthenticationApi.VerifyOtpAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/resetPassword", AffiliateAuthenticationApi.AffiliateForgotPassword)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapGet("/getUserProfile", AffiliateAuthenticationApi.GetUserProfileByIdAsync)
+                .AddRequireAuthorizationJWTToken(AuthoritiesConstants.AFFILIATE_PROFILE)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/updateUserProfile", AffiliateAuthenticationApi.UpdateUserProfileAsync)
+                .AddRequireAuthorizationJWTToken(AuthoritiesConstants.AFFILIATE_PROFILE)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/changePassword", AffiliateAuthenticationApi.ChangePasswordAsync)
+               .AddRequireAuthorizationJWTToken(AuthoritiesConstants.AFFILIATE_PROFILE)
+               .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/uploadAvatar", AffiliateAuthenticationApi.UploadAvatarAsync)
+              .AddRequireAuthorizationJWTToken(AuthoritiesConstants.AFFILIATE_PROFILE)
+              .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/refreshToken", AffiliateAuthenticationApi.RefreshTokenAsync)
+            .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/social/{type}/{providerId}", AffiliateAuthenticationApi.SocialAuthenAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/social/signup", AffiliateAuthenticationApi.SocialVerifyOtpSignupAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapGet("/social/redirectUrl/{provider:alpha}", AffiliateAuthenticationApi.GetSocialRedirectUrlAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapGet("/social/google/authen-callback", AffiliateAuthenticationApi.GoogleAuthenCallbackAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapGet("/social/apple/authen-callback", AffiliateAuthenticationApi.AppleAuthenCallbackAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapGet("/social/facebook/authen-callback", AffiliateAuthenticationApi.FacebookAuthenCallbackAsync)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            app.MapPost("/getuser-bylistusername", AffiliateAuthenticationApi.GetUserByListUserNamesAsync)
+                .AddRequireAuthorizationJWTToken(AuthoritiesConstants.USER)
+                .WithTags(SwaggerConstants.AffiliateAuthencationApi);
+
+            return app;
+        }
+
     }
 }
